5e414531ee6786bd7cbe36a1424fc15ac735346b
Andras Bacsai <andrasbacsai@users.noreply.github.com>
2019-07-16 23:53:33 +0200

    Updated coolAnalytics

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,16 +1,10 @@
 import CoolAnalytics from '@coollabsio/analytics'
 
-const analytics = new CoolAnalytics({
-    key: '1234123412341234',
-    type: 'frontend'
-})
-export default ({
-    router,
-  }) => {
-    router.afterEach((to) => {
-        analytics.log({
-            nextPage: to.fullPath
-        })
-      })
-  }
-  
+export default ({ router }) => {
+    const analytics = new CoolAnalytics({
+        key: '1234123412341234',
+        type: 'frontend'
+    })
+
+    router.afterEach((to) => { analytics.log({ nextPage: to.fullPath }) })
+}
