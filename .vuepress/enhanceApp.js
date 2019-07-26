b0d45467753797d0e4341a5bcfdcd552f166d8a1
Andras Bacsai <andrasbacsai@users.noreply.github.com>
2019-07-26 11:26:12 +0200

    Swithed API key

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -2,7 +2,7 @@ import CoolAnalytics from '@coollabsio/analytics'
 
 export default ({ router }) => {
     const analytics = new CoolAnalytics({
-        key: '1234123412341234',
+        key: '26a1b2c0-903d-4891-ba9c-6142f72fd692',
         type: 'frontend'
     })
 
